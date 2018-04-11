dd1f23a1a3c5787bf0d5ff3d4064370e454a37fe
薆 <237142681@qq.com>
2018-04-11 13:10:44 +0800

    添加图片灰色处理

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,27 +8,67 @@ JNICALL
 Java_com_example_ai_ndkdemo_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ 哈哈";
+    std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
 
 
-extern "C"
-JNIEXPORT jstring
-
-JNICALL
-Java_com_example_ai_ndkdemo_MainActivity_stringFromJNI2(
-        JNIEnv *env,
-        jobject) {
-    std::string hh = "方法二";
-    return env->NewStringUTF(hh.c_str());
-
-}
 extern "C"
 JNIEXPORT jint JNICALL
 Java_com_example_ai_ndkdemo_MainActivity_getSum(JNIEnv *env, jobject instance, jint a, jint b) {
 
     return a + b;
     // TODO
+
+}extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_ai_ndkdemo_MainActivity_stringFromJava(JNIEnv *env, jobject instance, jstring a_) {
+    const char *a = env->GetStringUTFChars(a_, 0);
+    if (NULL == a)
+        return env->NewStringUTF("发生错误");
+    char *b = "I from C++哈哈";
+    int len = strlen(a) + strlen(b);
+    char str[len];
+    strcpy(str, a);
+    strcat(str, b);
+    env->ReleaseStringUTFChars(a_, a);
+    return env->NewStringUTF(str);
+}
+
+
+//extern "C"{
+//jintArray JNICALL
+//Java_com_example_ai_ndkdemo_MainActivity_getImgToGray(JNIEnv *env, jobject instance,
+//                                                       jintArray data_, jint w, jint h);
+//}
+
+extern "C"
+JNIEXPORT jintArray JNICALL
+Java_com_example_ai_ndkdemo_MainActivity_getImgToGray(JNIEnv *env, jobject instance, jintArray data_,
+                                                      jint w, jint h) {
+
+    jint *data;
+    data = env->GetIntArrayElements(data_, NULL);
+    if (data == NULL) {
+        return 0; /* exception occurred */
+    }
+    int alpha = 0xFF << 24;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            // 获得像素的颜色
+            int color = data[w * i + j];
+            int red = ((color & 0x00FF0000) >> 16);
+            int green = ((color & 0x0000FF00) >> 8);
+            int blue = color & 0x000000FF;
+            color = (red + green + blue) / 3;
+            color = alpha | (color << 16) | (color << 8) | color;
+            data[w * i + j] = color;
+        }
+    }
+    int size=w * h;
+    jintArray result = env->NewIntArray(size);
+    env->SetIntArrayRegion(result, 0, size, data);
+    env->ReleaseIntArrayElements(data_, data, 0);
+    return result;
 
 }
--- app/src/main/java/com/example/ai/ndkdemo/MainActivity.java
+++ app/src/main/java/com/example/ai/ndkdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ai.ndkdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +23,74 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(""+getSum(2,5));
+        tv.setText("" + stringFromJava("I from Java哦"));
+
+        imageView = (ImageView) findViewById(R.id.image);
+        // 结果显示还是java更快...
+        System.out.println("start jni gray bitmap");
+        getJniBitmap();
+        System.out.println("end jni gray bitmap");
+        System.out.println("start java gray bitmap");
+        convertGrayImg();
+        System.out.println("end java gray bitmap");
+        imageView.setImageBitmap(getJniBitmap());
     }
+
+    // Java方法灰色话图片
+    public Bitmap convertGrayImg() {
+        Bitmap img1 = ((BitmapDrawable) getResources().getDrawable(R.mipmap.head)).getBitmap();
+
+        int w = img1.getWidth(), h = img1.getHeight();
+        int[] pix = new int[w * h];
+        img1.getPixels(pix, 0, w, 0, 0, w, h);
+
+        int alpha = 0xFF << 24;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                // 获得像素的颜色
+                int color = pix[w * i + j];
+                int red = ((color & 0x00FF0000) >> 16);
+                int green = ((color & 0x0000FF00) >> 8);
+                int blue = color & 0x000000FF;
+                color = (red + green + blue) / 3;
+                color = alpha | (color << 16) | (color << 8) | color;
+                pix[w * i + j] = color;
+            }
+        }
+        Bitmap result = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        result.setPixels(pix, 0, w, 0, 0, w, h);
+        return result;
+    }
+
+
+    // JNI方法灰色话图片
+    public Bitmap getJniBitmap() {
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.mipmap.head)).getBitmap();
+        int w = bitmap.getWidth(), h = bitmap.getHeight();
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+        int[] resultInt = getImgToGray(pix, w, h);
+        Bitmap resultImg = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+        resultImg.setPixels(resultInt, 0, w, 0, 0, w, h);
+        return resultImg;
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-    public native String stringFromJNI2();
-    public native int getSum(int a,int b);
+
+    // 测试int使用
+    public native int getSum(int a, int b);
+
+    // 测试String使用
+    // java内部是使用16bit的unicode编码（UTF-16）来表示字符串的，无论中文英文都是2字节；
+    //jni内部是使用UTF-8编码来表示字符串的，UTF-8是变长编码的unicode，一般ascii字符是1字节，中文是3字节；
+    //c/c++使用的是原始数据，ascii就是一个字节了，中文一般是GB2312编码，用两个字节来表示一个汉字。
+    public native String stringFromJava(String a);
+
+    // 图片处理为灰色
+    public native int[] getImgToGray(int[] data, int w, int h);
 }
